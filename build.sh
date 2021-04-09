3d28ed0ea962488d96403520982e2a7230b57c77
Niakr1s <pavel2188@gmail.com>
2021-04-09 20:25:01 +0300

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+rm -rf dist
 mkdir -p dist
 (cd client && npm run build:prod)
-go build -o=dist/main src/main.go
-GOOS=windows GOARCH=amd64 go build -o dist/main.exe src/main.go
+echo "building executables"
+go build -o=dist/vnr src/main.go
+GOOS=windows GOARCH=amd64 go build -o dist/vnr.exe src/main.go
