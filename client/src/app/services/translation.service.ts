7a76155922a10e1a4164f0536e16f8698f2c13b9
Niakr1s <pavel2188@gmail.com>
2021-04-14 18:56:27 +0300

    gui: removed from in url again

--- client/src/app/services/translation.service.ts
+++ client/src/app/services/translation.service.ts
@@ -46,7 +46,7 @@ export class TranslationService {
     try {
       const res = await this.http
         .get<TranslationResponse>(
-          `api/translate/${translator.name}?sentence=${sentence.sentence}&to=${lang.name}&from=ja`
+          `api/translate/${translator.name}?sentence=${sentence.sentence}&to=${lang.name}`
         )
         .toPromise();
       return Translation.create(translatorName, to, res.translation);
