3c204dbbf3bf9fe984712c2f5bb22d8184ab3546
Niakr1s <pavel2188@gmail.com>
2022-09-15 10:26:44 +0300

    added build.sh to ignore npm errors (if client is unchanged)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf dist
 mkdir -p dist
-(cd client && npm run build:prod)
+(cd client && npm run build:prod) || true
 echo "building executables"
 go build -o=dist/vnr src/main.go
 GOOS=windows GOARCH=amd64 go build -o dist/vnr.exe src/main.go
